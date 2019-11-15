f5ae9d3183b7406a78967126c3e1c2bdb181ca6c
Sam Hokin <shokin@ncgr.org>
2019-11-15 16:52:31 -0700

    Text on graph tweakage.

--- fr/plot.pca.hist.R
+++ fr/plot.pca.hist.R
@@ -3,6 +3,8 @@
 ##
 
 plot.pca.hist = function(pc) {
+    col.cases = rgb(1,0,0,1/2)
+    col.controls = rgb(0,0,1,1/2)
     sum.cases = sum(pca.ind$coord[cases,pc])/length(pca.ind$coord[cases,pc])
     sum.controls = sum(pca.ind$coord[controls,pc])/length(pca.ind$coord[controls,pc])
     cases.controls = sum.cases - sum.controls
@@ -13,10 +15,12 @@ plot.pca.hist = function(pc) {
     h.cases = hist(pca.ind$coord[cases,pc], breaks=breaks)
     h.controls = hist(pca.ind$coord[controls,pc], breaks=breaks)
     ymax = max(c(h.cases$counts,h.controls$counts))
-    plot(h.cases, col=rgb(1,0,0,1/2), xlab="PCA Coords", 
+    plot(h.cases, col=col.cases, xlab="PCA Coords", 
         main=paste("PC", pc, "Individual Coordinates", "cases - controls=", cases.controls),
 	ylim=c(0,ymax)
 	)
-    plot(h.controls, col=rgb(0,1,0,1/2), add=TRUE)
+    plot(h.controls, col=col.controls, add=TRUE)
+    legend(x="topleft", c("cases","controls","both"),
+    			fill=c(col.cases,col.controls,rgb(.5,0,.5)), bty="n")
 }
 
