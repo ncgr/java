195cc734694dd19e0a831237fa6488ad0b7d200e
Sam Hokin <shokin@ncgr.org>
2020-01-08 04:15:26 -0700

    Tweaks.

--- fr/src/main/java/org/ncgr/pangenomics/PangenomicGraph.java
+++ fr/src/main/java/org/ncgr/pangenomics/PangenomicGraph.java
@@ -24,7 +24,6 @@ import org.apache.commons.cli.Option;
 import org.apache.commons.cli.Options;
 import org.apache.commons.cli.ParseException;
 
-import org.jgrapht.ListenableGraph;
 import org.jgrapht.graph.DirectedAcyclicGraph;
 import org.jgrapht.alg.shortestpath.DijkstraShortestPath;
 
