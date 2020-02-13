5642074b58155a31fcbeb09fd660aafe4e2f7d2d
Sam Hokin <shokin@ncgr.org>
2020-02-12 18:58:05 -0700

    Several tweaks; put cases on top of controls.

--- gwas/plot.p.region.R
+++ gwas/plot.p.region.R
@@ -49,20 +49,20 @@ plot.p.region = function(chr="1", start=1, end=0, gene=NULL, label=FALSE, minCal
     lines(c(1,1e9), rep(2,2), col="gray", lty=2)
     
     ## highlight highly significant p values
-    if (hasCase) { points(seg$pos[ptsCase], seg$mlog10p[ptsCase], pch=19, cex=0.6, col="darkred") }
     if (hasControl) { points(seg$pos[ptsControl], seg$mlog10p[ptsControl], pch=19, cex=0.6, col="darkgreen") }
+    if (hasCase) { points(seg$pos[ptsCase], seg$mlog10p[ptsCase], pch=19, cex=0.6, col="darkred") }
 
     ## label them with position if requested
-    if (label && hasCase) {
-        text(seg$pos[ptsCase], seg$mlog10p[ptsCase], col="darkred", pos=4, cex=0.6, offset=0.2,
-             paste(seg$id[ptsCase]," ",seg$ref[ptsCase],seg$alts[ptsCase],
-                   " (",seg$caseVars[ptsCase],"/",seg$caseRefs[ptsCase],"|",seg$controlVars[ptsCase],"/",seg$controlRefs[ptsCase],";OR=",signif(seg$OR[ptsCase],3),")",sep="")
-             )
-    }
     if (label && hasControl) {
         text(seg$pos[ptsControl], seg$mlog10p[ptsControl], col="darkgreen", pos=4, cex=0.6, offset=0.2,
              paste(seg$id[ptsControl]," ",seg$ref[ptsControl],seg$alts[ptsControl],
                    " (",seg$caseVars[ptsControl],"/",seg$caseRefs[ptsControl],"|",seg$controlVars[ptsControl],"/",seg$controlRefs[ptsControl],";OR=",signif(seg$OR[ptsControl],3),")",sep="")
+             )
+    }
+    if (label && hasCase) {
+        text(seg$pos[ptsCase], seg$mlog10p[ptsCase], col="darkred", pos=4, cex=0.6, offset=0.2,
+             paste(seg$id[ptsCase]," ",seg$ref[ptsCase],seg$alts[ptsCase],
+                   " (",seg$caseVars[ptsCase],"/",seg$caseRefs[ptsCase],"|",seg$controlVars[ptsCase],"/",seg$controlRefs[ptsCase],";OR=",signif(seg$OR[ptsCase],3),")",sep="")
              )
     }
 
@@ -81,7 +81,8 @@ plot.p.region = function(chr="1", start=1, end=0, gene=NULL, label=FALSE, minCal
         } else {
             text((start+end)/2, ypos, ">")
         }
-    } else if (showGenes) {
+    }
+    if (showGenes) {
         ypos = par("yaxp")[1]
         bar = c(ypos-0.2, ypos+0.2)
         within = genes$seqid==chr & genes$end>=start & genes$start<=end
